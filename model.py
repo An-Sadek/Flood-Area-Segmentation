fb91ae5c5292cee6e02545cec94a9dde7d6d5f3a
An Hoang Nguyen <nguyenan.sadek@outlook.com>
2025-02-04 16:40:20 +0700

    Tao dataset

---
+++ model.py
@@ -0,0 +1,25 @@
+import tensorflow as tf 
+from tensorflow import keras
+from keras import layers
+
+
+class ConvBlock(layers.Layer):
+
+    def __init__(self):
+        pass
+
+    def call(self):
+        pass
+
+
+class UNet(layers.Layer):
+
+    def __init__(self):
+        pass
+
+    def call(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
